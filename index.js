3d1c66de1b5ff33d6587e082b84e5cd4e39e46a6
JJLee-20194099 <JJLee-20194099>
2021-10-29 17:04:11 +0700

    Learn three basic cases in emit and on methods

--- index.js
+++ index.js
@@ -12,11 +12,30 @@ var server = http.Server(app)
 const io = new Server(server);
 
 io.on("connection", function(socket) {
+
+    // Check connection from user
     console.log("Có người kết nối với socket_id: ", socket.id)
-    
     socket.on("disconnect", function() {
         console.log(`Opps!!! socket_id: ${socket.id} disconnected`)
     })
+
+    /*
+        Khi Client A gửi data
+
+        io.sockets.emit: Gửi lại toàn bộ client kể cả A
+        socket.emit: Chỉ gửi lại mình A (Ứng dụng khi Login)
+        socket.broadcast.emit: Gửi lại toàn bộ client trừ A
+    */
+
+    socket.on("Client-send-data", function(data) {
+        
+        // Test 3 cases
+        // io.sockets.emit("Server-send-data", data)
+        // socket.emit("Server-send-data", data)
+        
+        socket.broadcast.emit("Server-send-data", data)
+    })
+
 });
 
 
